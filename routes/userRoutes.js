2e8b7274b5a26a1674ce8bfbf3a3762da265eccf
Bukunmi Tanimonure <bukunmitanimonure@Bukunmis-MacBook-Pro.local>
2021-09-23 22:26:49 +0100

    add unit test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,9 +8,9 @@ router.post('/login', authController.login);
 router.post('/signup', authController.signup);
 
 // Protect all routes after this middleware
-router.use(authController.protect);
+//router.use(authController.protect);
 
-router.delete('/deleteMe', userController.deleteMe);
+//router.delete('/deleteMe', userController.deleteMe);
 
 // Only admin have permission to access for the below APIs 
 router.use(authController.restrictTo('admin'));
---
+++ test/user.js
@@ -0,0 +1,31 @@
+let mongoose = require("mongoose");
+let User = require("../models/userModel");
+ 
+//Require dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+ 
+chai.use(chaiHttp);
+//Our parent block
+describe('Users', () => {
+    beforeEach((done) => {
+        User.remove({}, (err) => {
+           done();
+        });
+    });
+ 
+    describe('/GET users', () => {
+        it('it should GET all the users', (done) => {
+            chai.request(server)
+                .get('/api/v1/users')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.status.should.be.eql('success');
+               done();
+            })
+        })
+    })
+})
